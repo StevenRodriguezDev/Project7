4d0c115e22560950d15123492ff596520a7e5ccc
StevenRodriguezDev <stevenrodriguez3913@gmail.com>
2022-04-21 21:30:18 -0600

    updatd js

--- web_app_dashboard_v3.2/js/app.js
+++ web_app_dashboard_v3.2/js/app.js
@@ -1,31 +1,30 @@
-
 const alertDiv = document.querySelector(".alerts");
 
 alertDiv.innerHTML = `<p><b>Alert:</b> You have unread messages</p>
- <p class="alert-x">x</p>`
+<p class="alert-x">x</p>`
 
  const alertX = document.querySelector(".alert-x");
- 
+
  const alertClose = alertX.addEventListener("click", (e) => {
-  alertDiv.style.display = "none";
-});
+     alertDiv.style.display = "none";
+ });
 
 const user = document.querySelector("#userField");
 const message = document.querySelector("#messageField");
 const sendBtn = document.querySelector("#sendMessage");
 
 sendBtn.addEventListener("click", (e) => {
-  if (user.value === "" && message.value === "") {
-      alert("Please fill out the user and the message fields before sending");
-  } else if (user.value === "") {
-      alert("Please fill out the user field before sending");
-  } else if (message.value === "") {
-      alert("Please fill out the message field before sending");
-  } else {
-      alert(`Message successfully sent to: ${user.value}`);
-      user.value = "";
-      message.value = "";
-  }
+    if (user.value === "" && message.value === "") {
+        alert("Please fill out the user and the message fields before sending");
+    } else if (user.value === "") {
+        alert("Please fill out the user field before sending");
+    } else if (message.value === "") {
+        alert("Please fill out the message field before sending");
+    } else {
+        alert(`Message successfully sent to: ${user.value}`);
+        user.value = "";
+        message.value = "";
+    }
 });
 
 const bell = document.querySelector(".bell");
@@ -45,23 +44,22 @@ const autocomplete = document.querySelector('.autocomplete');
 const autocompleteUl = document.querySelector('.autocomplete-users');
 let autocompleteChilds = document.querySelectorAll('.autocomplete-users li');
 
-
 const autocompleteEvent = userField.addEventListener('input', (e) => {
-  const userInput = userField.value.toLowerCase();
-  for (let i = 0; i < usernames.length; i++) {
-      const usernamesLowerCase = usernames[i].toLowerCase();
-      if (usernamesLowerCase.includes(userInput) & userInput !== '') {
-          addLi(usernames[i]);
-      } else {
-          removeLi(usernames[i]);
-      }
-  }
-  autocompleteChilds = document.querySelectorAll('.autocomplete-users li');
-  if (autocompleteChilds.length === 0) {
-      autocomplete.style.display = 'none';
-  } else {
-      autocomplete.style.display = 'block';
-  }
+    const userInput = userField.value.toLowerCase();
+    for (let i = 0; i < usernames.length; i++) {
+        const usernamesLowerCase = usernames[i].toLowerCase();
+        if (usernamesLowerCase.includes(userInput) & userInput !== '') {
+            addLi(usernames[i]);
+        } else {
+            removeLi(usernames[i]);
+        }
+    }
+    autocompleteChilds = document.querySelectorAll('.autocomplete-users li');
+    if (autocompleteChilds.length === 0) {
+        autocomplete.style.display = 'none';
+    } else {
+        autocomplete.style.display = 'block';
+    }
 });
 
 const autocompleteFocus = userField.addEventListener('focus', (e) => {
@@ -72,7 +70,28 @@ const autocompleteFocus = userField.addEventListener('focus', (e) => {
     }
 });
 
-
 const autocompleteBlur = userField.addEventListener('blur', (e) => {
-  autocomplete.style.display = 'none';
+    autocomplete.style.display = 'none';
 });
+
+const addLi = (name) => {
+    if (autocompleteChilds.length === 0) {
+        const li = document.createElement('LI');
+        li.innerHTML = name;
+        autocompleteUl.appendChild(li);
+    } else {
+        let x = 0;
+        for (let i = 0; i < autocompleteChilds.length; i++) {
+            const li = autocompleteChilds[i];
+            
+            if (li.innerHTML === name) {
+                x++;
+            }
+        }
+        if (x === 0) {
+            const li = document.createElement('LI');
+            li.innerHTML = name;
+            autocompleteUl.appendChild(li);        
+        } 
+    }
+};
