6a28d0bf9ec8a04463ca04cbff050eee271f71f8
StevenRodriguezDev <stevenrodriguez3913@gmail.com>
2022-04-21 22:44:59 -0600

    updated grpahs

--- web_app_dashboard_v3.2/js/graphs.js
+++ web_app_dashboard_v3.2/js/graphs.js
@@ -1,8 +1,10 @@
-const trafficCanvas = document.getElementById('traffic-chart');
+const trafficCanvas = document.getElementById("traffic-chart");
 let trafficData = {
-    labels: ["16-22", "23-29", "30-5", "6-12", "13-9", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+    labels: ["16-22", "23-29", "30-5", "6-12", "13-19", "20-26", "27-3",
+        "4-10", "11-17", "18-24", "25-31"],
     datasets: [{
-        data: [750,1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500, 2500],
+        data: [750, 1250, 1000, 2000, 1500, 1750, 1250, 1850, 2250, 1500,
+            2500],
         backgroundColor: 'rgba(116, 119, 191, .5)',
         borderWidth: 1,
         fill: true,
@@ -10,6 +12,8 @@ let trafficData = {
         tension: .4,
     }]
 }
+
+
 
 let trafficOptions = {
     aspectRatio: 2.5,
@@ -24,12 +28,65 @@ let trafficOptions = {
         }
     }
 };
-
 let trafficChart = new Chart(trafficCanvas, {
     type: 'line',
-    date: trafficData,
-    options: trafficOptions
+    data: trafficData,
+    
 });
 
+const dailyCanvas = document.getElementById("daily-chart");
+const dailyChartData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: '#7477BF',
+        borderWidth: 1
+    }]
+};
+
+let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyChartData,
+    
+});
+
+
+
+const mobileCanvas = document.getElementById("mobile-users-chart");
+const mobileData = {
+    labels: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: '# of Users',
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            '#7477BF',
+            '#81C98F',
+            '#51B6C8'
+        ]
+    }]
+};
+const mobileOptions = {
+    plugins: {
+        legend: {
+            position: 'right',
+            labels: {
+                boxWidth: 20,
+                font: {
+                    weight: 700,
+                }
+            }
+        }
+    }
+};
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData,
+    
+});
+
+
+
+const trafficRadios = document.getElementsByClassName('radio')
 
 
